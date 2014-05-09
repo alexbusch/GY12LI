9e2dbca638b00ce113b867fa29bce88dafb46473
Marc Reis <Marc.Reis.79@googlemail.com>
2014-05-09 12:24:47 +0200

    Create KommazahlenGrenzen.java

---
+++ KommazahlenGrenzen.java
@@ -0,0 +1,15 @@
+public class KommazahlenGrenzen{
+public static void main(String[] args){
+  float f = 42.0f;
+  double d = 42.0d;
+  
+  System.out.println(f);
+  System.out.println(d);
+  
+  System.out.println(Float.MIN_VALUE);
+  System.out.println(Float.MAX_VALUE);
+  System.out.println(Double.MIN_VALUE);
+  System.out.println(Double.MAX_VALUE);
+  
+  }
+}

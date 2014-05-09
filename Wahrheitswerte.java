9227079dd43e61a977686beee48e3bd931ea9b10
Marc Reis <Marc.Reis.79@googlemail.com>
2014-05-09 12:31:42 +0200

    Update Wahrheitswerte.java

--- Wahrheitswerte.java
+++ Wahrheitswerte.java
@@ -1,4 +1,12 @@
 public class Wahrheitswerte{
-
+  
+  public static void main (String args[]){
+    boolean bot = true;
+    boolean bof = false;
+    
+    System.out.println(bot);
+    System.out.println(bof);
+    
+  }
 
 }

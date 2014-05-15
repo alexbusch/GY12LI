fd4739e0ca071e5a2a9e3fdc4e7737848416c178
reisma <reisma@reisma-7-VMPC.gy.bnt.intra>
2014-05-15 12:28:43 +0200

    Einstiegsbeispiel zu einem einfachen Taschenrechner

--- GY12LI/src/gy12li/Taschenrechner.java
+++ GY12LI/src/gy12li/Taschenrechner.java
@@ -5,7 +5,6 @@
 package gy12li;
 
 import java.io.Console;
-import java.util.logging.ConsoleHandler;
 
 /**
  *
@@ -13,38 +12,64 @@ import java.util.logging.ConsoleHandler;
  */
 public class Taschenrechner {
 
+    static String programmname = "Taschenrechner";
+    static String autoren = "Reis";
+    static double eingabe;
+    static float version = 0.1f;
+    static Console con = System.console();
+
+    /**
+     * Start Methode, zeigt in einer Schleife eine Auswahl an
+     * Berechnungsoptionen an und Berechnet diese Wird -1 eingegeben Endet das
+     * Programm
+     *
+     * @param args
+     */
     public static void main(String args[]) {
-        Taschenrechner meiner = new Taschenrechner();
-    }
-    
-    String programmname = "Taschenrechner";
-    String autoren = "Reis";
-    double eingabe;
-    float version = 0.1f;
 
-    public Taschenrechner() {
+        System.out.println(programmname + " " + version);
+        
+        //Eine Schleife, Sie zeigt das menü immer wieder an und berechnet...
         while (eingabe != -1) {
-            System.out.println(programmname + " " + version);
+            
+            // Anzeige des Menüs 1 für Addieren, 2 für...
             System.out.println("Was wollen Sie Berechnen? \n 1)+ \n 2)- \n 3)* \n 4)\\");
-
-            // eingabe = System.console().readLine();
+            
+            // Zahl einelesen, siehe Funktion zahlEinlesen()
             eingabe = zahlEinlesen();
             System.out.println("Sie haben :" + eingabe + " gewählt");
 
+            // Es wurde eien 1 eingegeben -> wir addieren
             if (eingabe == 1) {
                 System.out.println("Sie wollen addieren, geben Sie Zahl 1 und Zahl 2 ein");
                 System.out.println("Ergebnis: "
-                        + addiere(zahlEinlesen(), zahlEinlesen()));
+                        + addiere(zahlEinlesen(), zahlEinlesen()) +"\n");
             }
         }
     }
 
-    double zahlEinlesen() {
-        //return Double.valueOf( System.console().readLine() );
-        return Double.valueOf(javax.swing.JOptionPane.showInputDialog("Bitte Zahl eingeben"));
+    /**
+     * Liest in der echten Console mit System.console eine eingabe ein. Ist
+     * diese null dann mit einem JOptionPane (z.B. in NetBeans)
+     *
+     * @return eingelesene Zahl als double
+     */
+    static double zahlEinlesen() {
+        if (con != null) {
+            return Double.valueOf(System.console().readLine());
+        } else {
+            return Double.valueOf(javax.swing.JOptionPane.showInputDialog("Bitte Zahl eingeben"));
+        }
     }
 
-    double addiere(double a, double b) {
+    /**
+     * Addiert zwei Zahlen
+     *
+     * @param a Zahl 1
+     * @param b Zahl 2
+     * @return addierte Werte
+     */
+    static double addiere(double a, double b) {
 
         return a + b;
     }
